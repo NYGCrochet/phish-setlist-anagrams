af03d81b196012043759c0e2797fb0830d2f487d
Jeff Lang <jeffplang@gmail.com>
2013-09-07 20:30:08 -0400

    added command line option to preserve articles at the beginning of song titles

--- setlist_anagram.rb
+++ setlist_anagram.rb
@@ -3,21 +3,22 @@ require_relative 'lib/show'
 class SetlistAnagram
   attr_accessor :titles
 
-  def initialize date
+  def initialize(date, opts = {})
     @show = Show.new date
+    @opts = opts
 
     set_clean_titles
   end
 
   def set_clean_titles
-    @titles = @show.songs
-                .map!(&:upcase)
-                .map!{ |t| t.gsub(/^THE |^A /, '') }
-                .map!{ |t| t.gsub(/[^0-9A-Z ]/i, '') }
+    @titles = @show.songs.map(&:upcase)
+
+    @titles.map!{ |t| t.gsub(/^THE |^A /, '') } unless @opts[:'preserve-articles']
+    @titles.map!{ |t| t.gsub(/[^0-9A-Z ]/i, '') }
   end
 
-  def pp_titles(opts = {})
-    if opts[:reverse]
+  def pp_titles()
+    if @opts[:reverse]
       titles = @titles.reverse
     else
       titles = @titles
@@ -26,7 +27,7 @@ class SetlistAnagram
     titles
       .map{ |t| t.gsub(/\s/, '') }
       .each do |t| 
-        title = opts[:'reverse-letters'] ? t.reverse : t
+        title = @opts[:'reverse-letters'] ? t.reverse : t
         puts title.split('').join(' ')
       end
   end
@@ -42,8 +43,9 @@ if __FILE__ == $0
   opts = Trollop::options do
     opt :reverse, "Display song list in reverse"
     opt :'reverse-letters', "Display song titles in reverse", {:short => 'l'}
+    opt :'preserve-articles', "Preserve 'The' and 'A' at the beginning of song titles"
   end
 
-  sa = SetlistAnagram.new ARGV[0]
-  sa.pp_titles(opts)
+  sa = SetlistAnagram.new ARGV[0], opts
+  sa.pp_titles
 end
